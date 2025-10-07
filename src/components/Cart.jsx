8fcfa7d5993694355cd68c192bd53a47a57c67a1
SriVenkatG <srivenkat1357@gmail.com>
2025-10-07 11:56:20 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://3.84.243.8:8083/back1';
+const BASE_URL = 'http://localhost:30083/back1';
 
 const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
